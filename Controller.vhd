0e45b3be75cb2c59404eaba2817a889fa8f44357
Chrispapge96 <chrpapge@gmail.com>
2022-01-29 01:02:58 +0100

    needs work

--- Controller.vhd
+++ Controller.vhd
@@ -21,23 +21,91 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity Controller is
---  Port ( );
+  port(
+  		clk:		in  std_logic;
+  		rst:		in  std_logic;
+		IN_read : 	in  std_logic;
+		IN_load: 	in  std_logic;
+		IN_matrix:	in  std_logic_vector(3 downto 0);
+		web: 		out std_logic_vector(1 downto 0);
+		addr_ram_w:	out std_logic_vector(7 downto 0);
+		addr_ram_r:	out std_logic_vector(7 downto 0);
+		cnt_enable: out std_logic;
+		addr_in:	out std_logic_vector(3 downto 0);
+		addr_rom:	out std_logic_vector(3 downto 0);
+		cnt:		out std_logic_vector(5 downto 0);
+		rst_sumReg:	out std_logic
+
+		);
+
 end Controller;
 
 architecture Behavioral of Controller is
+ 
+	type state_of_operation is (IDLE,OP,LOAD,SAVE,LINE_UPDATE,FINISHED);
+	type state_of_loading is (WAIT_LOAD,FETCH);
+	signal state_cur, state_next: 		state_of_operation;
+	signal loading_cur,loading_next : 	state_of_loading;
 
-begin
+	signal sumReg_c,sumReg_n : 					std_logic_vector(15 downto 0);
+	signal web :						std_logic_vector(1 downto 0);
+	signal cnt_r,cnt_n : 				std_logic_vector(5 downto 0);
+
+
+	begin
+
+	seq: process(clk,rst,state_next,loading_next) is begin
+		if rising_edge(clk) then
+			if rst = '1' then
+				state_cur<=IDlE;
+				loading_cur<=WAIT_LOAD;
+			else
+				state_cur<=state_next;
+				loading_cur<=loading_next;
+				-- goes to MAC unit sumReg_c<=sumReg_n;
+			end if;
+		end if;
+	end process;
+
+
+
+	end process;
+
+	Operation_of each state: process() is begin
+	state_next<=state_cur;
+	loading_next<=loading_cur;
+		case state_cur is
+			when IDLE =>
+				rst_sumReg<='1';
+				-- goes to Input unit input_reg_next(255 downto 0)<=(others=>'0');
+				cnt_n<='0';
+				addr_in<="0000";
+				addr_rom<="0000";
+				web<="00";
+				-- goes to MAC unit sumReg_n<=(others=>'0'); 
+				addr_ram_r <= IN_matrix & "0000";
+				finished<='0';
+				if IN_read='1' then
+					state_next<=READ;
+				else
+					state_next<=state_cur;
+					if IN_load='1' then
+					state_next<=LOAD;
+				end if;
+			when
+
+
+
+
+
+
+
+
+
+
 
 
 end Behavioral;
--- TOP_Accelerator.vhd
+++ TOP_Accelerator.vhd
@@ -1,41 +1,39 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: GROUP 7
+-- Engineer: ALEC GUERIN & CHRISTOS PAPADOPOULOS
 -- 
 -- Create Date: 01/28/2022 07:07:07 PM
 -- Design Name: 
 -- Module Name: TOP_Accelerator - Structural
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity TOP_Accelerator is
 --  Port ( );
 end TOP_Accelerator;
 
 architecture Structural of TOP_Accelerator is
+
+component Controller is
+	port(
+		clk:		in std_logic;
+		rst:		in std_logic;
+		IN_read : 	in std_logic;
+		IN_load: 	in std_logic;
+		web: 		out std_logic_vector(1 downto 0);
+		addr_ram_w:	out std_logic_vector(7 downto 0);
+		addr_ram_r:	out std_logic_vector(7 downto 0);
+		cnt_enable: out std_logic;
+		addr_in:	out std_logic_vector(3 downto 0);
+		addr_rom:	out std_logic_vector(3 downto 0);
+		cnt:		out std_logic_vector(5 downto 0);
+
+		)
 
 begin
 
