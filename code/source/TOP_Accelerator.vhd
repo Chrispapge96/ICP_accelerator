a6007febda2cc417e97a5e1a21b4def33c0679bd
AlecGuerin <66556227+AlecGuerin@users.noreply.github.com>
2022-01-29 14:29:12 +0100

    Add vivado project
    Test Vivado project export.

--- code/source/TOP_Accelerator.vhd
+++ code/source/TOP_Accelerator.vhd
@@ -14,10 +14,21 @@ use IEEE.NUMERIC_STD.ALL;
 
 
 entity TOP_Accelerator is
---  Port ( );
+	Port (
+		clk        : in  std_logic;                       -- Clock signal
+		reset      : in  std_logic;                       -- Reset signal
+		IN_read    : in  std_logic;                       -- Read signal
+		IN_load    : in  std_logic;                       -- Start loading data signal
+		IN_data_in : in  std_logic_vector(15 downto 0);   -- Input data to set
+		IN_matrix_sel : in std_logic_vector(3 downto 0);  -- Result matrix index
+		OUT_data_out : out std_logic_vector(15 downto 0);  -- Output data
+		finish     : out std_logic
+	);
 end TOP_Accelerator;
 
 architecture Structural of TOP_Accelerator is
+
+---- COMPONENT ----
 
 component Controller is
 	port(
@@ -31,11 +42,15 @@ component Controller is
 		cnt_enable: out std_logic;
 		addr_in:	out std_logic_vector(3 downto 0);
 		addr_rom:	out std_logic_vector(3 downto 0);
-		cnt:		out std_logic_vector(5 downto 0);
+		cnt:		out std_logic_vector(5 downto 0)
+		);
+end component;
 
-		)
+---- SIGNAL DEFINITIONS --
+
 
 begin
-
+	OUT_data_out <= (others => '0');
+	finish <= '0';
 
 end Structural;
