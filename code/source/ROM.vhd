a899058f97da9b865060722a180fd77821f46317
Chris Papado <chrpapge@gmail.com>
2022-02-02 17:28:23 +0100

    Added ROM

---
+++ code/source/ROM.vhd
@@ -0,0 +1,45 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_arith.all;
+
+entity ROM is
+ Port ( clk         : in std_logic;
+        enable_ROM  : in std_logic;
+        addr_ROM    : in std_logic_vector (3 downto 0); 
+        dataROM     : out std_logic_vector (11 downto 0)
+        );
+end ROM;
+
+architecture Behavioral of ROM is
+
+    type ROM_Memory is array (0 to 15) of std_logic_vector(11 downto 0);
+    
+    constant coef : ROM_Memory:=(
+        "000011010110",
+        "001011000001",
+        "001000000011",
+        "000001000010",
+	    "001000001111",
+	    "000010000100",
+	    "001100000110",
+	    "000001000010",
+	    "010010101000",
+	    "000011000010",
+	    "010000001001",
+	    "000001000010",
+	    "000001001010",
+	    "000100000000",
+	    "000010001100",
+	    "000001000010"
+               );
+      
+    
+    begin
+    
+   seq: process(clk) is begin
+            if rising_edge(clk) and enable_ROM='1' then
+                dataROM<=coef(CONV_INTEGER(unsigned(addr_ROM)));
+            end if;
+        end process;
+    
+end Behavioral;
--- code/source/TOP_Accelerator.vhd
+++ code/source/TOP_Accelerator.vhd
@@ -32,20 +32,29 @@ architecture Structural of TOP_Accelerator is
 
 component Controller is
 	port(
-		clk           :	in std_logic;
-		rst           :	in std_logic;
-		IN_read       : in std_logic;
-		IN_load       : in std_logic;
-		IN_matrix     :	in  std_logic_vector(3 downto 0);
-		web           : out std_logic_vector(1 downto 0);
-		addr_ram      :	out std_logic_vector(7 downto 0);
-		addr_In       :	out std_logic_vector(3 downto 0);
-		addr_rom      :	out std_logic_vector(3 downto 0);
-		rst_sumReg    :	out std_logic;
-		load_enable   : out std_logic;
-		finish        : out std_logic
-		
+            clk           :	in std_logic;
+            rst           :	in std_logic;
+            IN_read       : in std_logic;
+            IN_load       : in std_logic;
+            IN_matrix     :	in  std_logic_vector(3 downto 0);
+            web           : out std_logic_vector(1 downto 0);
+            addr_ram      :	out std_logic_vector(7 downto 0);
+            addr_In       :	out std_logic_vector(3 downto 0);
+            addr_rom      :	out std_logic_vector(3 downto 0);
+            rst_sumReg    :	out std_logic;
+            load_enable   : out std_logic;
+            finish        : out std_logic
+            
 		);
+end component;
+
+component ROM is
+     Port ( 
+            clk         : in std_logic;
+            enable_ROM  : in std_logic;
+            addr_ROM    : in std_logic_vector (3 downto 0); 
+            dataROM     : out std_logic_vector (11 downto 0)
+        );
 end component;
 
 ---- SIGNAL DEFINITIONS --
@@ -55,26 +64,38 @@ signal	addr_In: std_logic_vector(3 downto 0);
 signal	addr_rom: std_logic_vector(3 downto 0);
 signal	rst_sumReg: std_logic;
 signal  load_enable: std_logic;
+signal  enable_ROM: std_logic;
+signal  dataROM:    std_logic_vector(11 downto 0);
 
 
 begin
 --	OUT_data_out <= (others => '0');
 --	finish <= '0';
-
+    enable_ROM<='1'; -- Not sure if we need this, but to be sure i have it prepared
+	
 	contr: Controller
 	port map (
-		clk=>clk,
-		rst=>rst,
-		IN_load=>IN_load,
-		IN_read=>IN_read,
-		IN_matrix=>IN_matrix,
-		web=>web,
-		addr_ram=>addr_ram,
-		addr_In=>addr_In,
-		addr_rom=>addr_rom,
-		rst_sumReg=>rst_sumReg,
-		load_enable=>load_enable,
-		finish=>finish
+            clk=>clk,
+            rst=>rst,
+            IN_load=>IN_load,
+            IN_read=>IN_read,
+            IN_matrix=>IN_matrix,
+            web=>web,
+            addr_ram=>addr_ram,
+            addr_In=>addr_In,
+            addr_rom=>addr_rom,
+            rst_sumReg=>rst_sumReg,
+            load_enable=>load_enable,
+            finish=>finish
 	);
+	
+	 rom_mem: ROM
+	 port map (
+             clk=>clk,
+             enable_ROM=>enable_ROM,
+             addr_ROM=>addr_rom,
+             dataROM=>dataROM	 
+	 );
+	
 
 end Structural;
