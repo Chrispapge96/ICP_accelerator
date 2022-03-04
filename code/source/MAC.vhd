70aeab46fed7d3c288eb8dab1e7df2e48e8b1ea3
Chris Papado <chrpapge@gmail.com>
2022-03-04 22:16:09 +0100

    Update MAC.vhd

--- code/source/MAC.vhd
+++ code/source/MAC.vhd
@@ -83,7 +83,7 @@ begin
             end if;
 
             if en_diag='1' then
-                diag_sum_n<=std_logic_vector(unsigned(diag_sum_c)+ unsigned("00" & mac_n));
+                diag_sum_n<=std_logic_vector(unsigned(diag_sum_c)+ unsigned("00" & mac_c));
             end if;
 
             
